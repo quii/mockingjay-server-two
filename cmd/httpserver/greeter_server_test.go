cc14ba67c5580a139adebc566cf7a1fa8e1b20cb
Chris James <quii@hey.com>
2022-11-05 12:18:33 +0000

    iteration zero: - hello world server - acceptance test

---
+++ cmd/httpserver/greeter_server_test.go
@@ -0,0 +1,30 @@
+package main_test
+
+import (
+	"fmt"
+	"net/http"
+	"testing"
+	"time"
+
+	"github.com/quii/mockingjay-server-two/adapters"
+	"github.com/quii/mockingjay-server-two/adapters/httpserver"
+	"github.com/quii/mockingjay-server-two/specifications"
+)
+
+func TestGreeterServer(t *testing.T) {
+	if testing.Short() {
+		t.Skip()
+	}
+	var (
+		port   = "8080"
+		driver = httpserver.Driver{
+			BaseURL: fmt.Sprintf("http://localhost:%s", port),
+			Client: &http.Client{
+				Timeout: 1 * time.Second,
+			},
+		}
+	)
+
+	adapters.StartDockerServer(t, port, "httpserver")
+	specifications.GreetSpecification(t, driver)
+}
